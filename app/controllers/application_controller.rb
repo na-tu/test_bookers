44d24375ee417e7d6a17b16f2b774215e6c01067
na-tu <161991245+na-tu@users.noreply.github.com>
2024-10-01 14:16:59 +0000

    2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,18 @@
 class ApplicationController < ActionController::Base
    before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    about_path
+  end#ログインした後にアバウトにいく。
+
+ def after_sign_out_path_for(resource)
+    about_path
+ end#ログアウトした後にアバウト
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
+
 end
